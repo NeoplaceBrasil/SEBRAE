40424aba8c15f7158bcb40797e4328b2fcdeed4e
paulrsilva <paulino.rsilva@gmail.com>
2019-04-10 14:22:33 -0300

    Estruturação de Projeto e Requisitos

---
+++ portal/clube_descontos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClubeDescontosConfig(AppConfig):
+    name = 'clube_descontos'
---
+++ portal/encontre/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EncontreConfig(AppConfig):
+    name = 'encontre'
